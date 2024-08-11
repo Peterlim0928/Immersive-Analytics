b97b28f0926d9602bffce8be398cd064407cc7ae
ycha0132 <ycha0132@student.monash.edu>
2024-08-11 18:42:24 +0800

    Add Filtering Min Max Features for 3-Dimensions

--- Immersive Analytics Unity/Assets/IATK/Scripts/Controller/VRMenuInteractor.cs
+++ Immersive Analytics Unity/Assets/IATK/Scripts/Controller/VRMenuInteractor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace IATK
 {
@@ -15,39 +16,50 @@ namespace IATK
         public Dropdown Y_AxisDropDown;
         public Dropdown Z_AxisDropDown;
         public Dropdown SizeAttributeDropDown;
-        public Dropdown FilterAttributeDropDown;
-        public Slider FilterMinSlider;
-        public Slider FilterMaxSlider;
+        public TMP_Dropdown DataSourceDropDown;
+        
+        /**
+        * Sliders for filtering the data
+        */
+        public Slider X_Axis_SliderMinFilter;
+        public Slider X_Axis_SliderMaxFilter;
 
+        public Slider Y_Axis_SliderMinFilter;
+        public Slider Y_Axis_SliderMaxFilter;
+
+        public Slider Z_Axis_SliderMinFilter;
+        public Slider Z_Axis_SliderMaxFilter;
 
         //holds the string names of the data attributes
         private List<string> DataAttributesNames;
 
+        // stores the list of data source options
+        private List<string> DataSourceNames;
+
         // Use this for initialization
         void Start()
         {
+            // Retrieve Data Source Names
+            DataSourceNames = GetDataSourceNames();
+            DataSourceDropDown.AddOptions(DataSourceNames);
+            
             DataAttributesNames = GetAttributesList();
-            X_AxisDropDown.AddOptions(DataAttributesNames);
-            Y_AxisDropDown.AddOptions(DataAttributesNames);
-            Z_AxisDropDown.AddOptions(DataAttributesNames);
-
-            X_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.xDimension.Attribute);
-            Y_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.yDimension.Attribute);
-            Z_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.zDimension.Attribute);
-
-            SizeAttributeDropDown.AddOptions(DataAttributesNames);
-            FilterAttributeDropDown.AddOptions(DataAttributesNames);
-
-            SizeAttributeDropDown.value = DataAttributesNames.IndexOf(visualisation.sizeDimension);
-
-            if(visualisation.attributeFilters.Length>0)
-            FilterAttributeDropDown.value = DataAttributesNames.IndexOf(visualisation.attributeFilters[0].Attribute);
+            PopulateAttributeDropdowns();
+            SetInitialDropDownValues();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private List<string> GetDataSourceNames()
+        {
+            return new List<string> {
+                "msft_1y.csv",
+                "cereal.csv"
+            };
         }
 
         private List<string> GetAttributesList()
@@ -59,6 +71,24 @@ namespace IATK
                 dimensions.Add(visualisation.dataSource[i].Identifier);
             }
             return dimensions;
+        }
+
+        private void PopulateAttributeDropdowns()
+        {
+            X_AxisDropDown.AddOptions(DataAttributesNames);
+            Y_AxisDropDown.AddOptions(DataAttributesNames);
+            Z_AxisDropDown.AddOptions(DataAttributesNames);
+            SizeAttributeDropDown.AddOptions(DataAttributesNames);
+        }
+
+        private void SetInitialDropDownValues()
+        {
+            X_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.xDimension.Attribute);
+            Y_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.yDimension.Attribute);
+            Z_AxisDropDown.value = DataAttributesNames.IndexOf(visualisation.zDimension.Attribute);
+
+            SizeAttributeDropDown.value = DataAttributesNames.IndexOf(visualisation.sizeDimension);
+
         }
 
         public void ValidateX_AxisDropdown()
@@ -108,34 +138,57 @@ namespace IATK
             }
         }
 
-        public void ValidateAttributeFilteringDropDown()
+        public void ValidateXAxisSliderMinFilter()
         {
             if (visualisation != null)
             {
-                AttributeFilter af = new AttributeFilter();
-                af.Attribute = DataAttributesNames[FilterAttributeDropDown.value];
-                visualisation.attributeFilters[0] = af;
-                visualisation.updateViewProperties(AbstractVisualisation.PropertyType.AttributeFiltering);
+                visualisation.xDimension.minFilter = X_Axis_SliderMinFilter.value;
+                visualisation.updateProperties();
             }
         }
 
-        public void ValidateAttributeFilteringSliderMin()
+        public void ValidateXAxisSliderMaxFilter()
         {
-            if (visualisation != null && visualisation.attributeFilters.Length>0)
+            if (visualisation != null)
             {
-                visualisation.attributeFilters[0].minFilter = FilterMinSlider.value;
-                visualisation.updateViewProperties(AbstractVisualisation.PropertyType.AttributeFiltering);
-
+                visualisation.xDimension.maxFilter = X_Axis_SliderMaxFilter.value;
+                visualisation.updateProperties();
             }
         }
 
-        public void ValidateAttributeFilteringSliderMax()
+        public void ValidateYAxisSliderMinFilter()
         {
-            if (visualisation != null && visualisation.attributeFilters.Length > 0)
+            if (visualisation != null)
             {
-                visualisation.attributeFilters[0].maxFilter = FilterMaxSlider.value;
-                visualisation.updateViewProperties(AbstractVisualisation.PropertyType.AttributeFiltering);
+                visualisation.yDimension.minFilter = Y_Axis_SliderMinFilter.value;
+                visualisation.updateProperties();
+            }
+        }
+        
+        public void ValidateYAxisSliderMaxFilter()
+        {
+            if (visualisation != null)
+            {
+                visualisation.yDimension.maxFilter = Y_Axis_SliderMaxFilter.value;
+                visualisation.updateProperties();
+            }
+        }
 
+        public void ValidateZAxisSliderMinFilter()
+        {
+            if (visualisation != null)
+            {
+                visualisation.zDimension.minFilter = Z_Axis_SliderMinFilter.value;
+                visualisation.updateProperties();
+            }
+        }
+
+        public void ValidateZAxisSliderMaxFilter()
+        {
+            if (visualisation != null)
+            {
+                visualisation.zDimension.maxFilter = Z_Axis_SliderMaxFilter.value;
+                visualisation.updateProperties();
             }
         }
     }
