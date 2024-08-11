ddc295ab829a3721dc0d9582f8ed77bbaaa3fbf7
jlim0215 <limjiantao@gmail.com>
2024-08-11 18:51:21 +0800

    Added Move Feature
    - Added the ability for the user to enter move mode and move the graphs around
    - Added temporary objects to the scene for references

--- Immersive Analytics Unity/Assets/Scripts/MenuController.cs
+++ Immersive Analytics Unity/Assets/Scripts/MenuController.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class MenuController : MonoBehaviour
 {
@@ -12,6 +12,9 @@ public class MenuController : MonoBehaviour
 
     // The prefab to create
     public GameObject visualisationPrefab;
+
+    private bool isMoveMode = false;
+    private bool isDeleteMode = false;
 
     private void Start()
     {
@@ -24,9 +27,61 @@ public class MenuController : MonoBehaviour
         gameObject.SetActive(isMenuShown);
     }
 
-    public void CreateObject()
+    public void OnCreateButtonPressed()
     {
         Transform spawnLocation = Camera.main.transform;
         Instantiate(visualisationPrefab, spawnLocation.position, spawnLocation.rotation);
+    }
+
+    public void OnMoveButtonPressed()
+    {
+        isMoveMode = !isMoveMode;
+        isDeleteMode = false;
+
+        GameObject[] moveableObjects = GameObject.FindGameObjectsWithTag("Moveable");
+
+        if (isMoveMode)
+        {
+            // Enable the move mode
+            Debug.Log("Move mode enabled");
+
+            foreach (GameObject moveableObject in moveableObjects)
+            {
+                var grabInteractable = moveableObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+                if (grabInteractable == null)
+                {
+                    grabInteractable = moveableObject.AddComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+                }
+                grabInteractable.throwOnDetach = false;
+            }
+        }
+        else
+        {
+            // Disable the move mode
+            Debug.Log("Move mode disabled");
+
+            foreach (GameObject moveableObject in moveableObjects)
+            {
+                var grabInteractable = moveableObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+                Destroy(grabInteractable);
+            }
+        }
+    }
+
+    public void OnDeleteButtonPressed()
+    {
+        isMoveMode = false;
+        isDeleteMode = !isDeleteMode;
+
+        if (isDeleteMode)
+        {
+            // Enable the delete mode
+            Debug.Log("Delete mode enabled");
+        }
+        else
+        {
+            // Disable the delete mode
+            Debug.Log("Delete mode disabled");
+        }
     }
 }
