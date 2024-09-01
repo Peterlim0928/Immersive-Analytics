7a7832f39b5c6383a9b0bfb5c4d2ece2afed8aa0
jlim0212 <jlim0212@student.monash.edu>
2024-09-01 17:07:51 +0800

    Added stock data time interval to the script

--- Immersive Analytics 2 Unity/Assets/Scripts/script.py
+++ Immersive Analytics 2 Unity/Assets/Scripts/script.py
@@ -2,10 +2,10 @@ import sys
 import yfinance as yf
 import pandas as pd
 
-def main(stock_code: str, time_period: str):
+def main(stock_code: str, time_period: str, stock_interval: str = "30m"):
     stock_code = stock_code.upper()
     stock_data = yf.Ticker(stock_code)
-    hist = stock_data.history(period=time_period)
+    hist = stock_data.history(period = time_period, interval = stock_interval)
     pd.DataFrame(hist).to_csv(f"./Assets/Datasets/{stock_code}-{time_period}.csv")
 
 
